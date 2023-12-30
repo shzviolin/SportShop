2853359b21c4e37132750fe03beb12b23c19a52d
Ali Akbar Ebrahimi <a.ebrahimi1367@gmail.com>
2023-12-30 19:51:45 +0330

    01_Initialize the project

---
+++ API/Controllers/ProductController.cs
@@ -0,0 +1,27 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace API.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class ProductController : ControllerBase
+    {
+        [HttpGet]
+        public string GetProducts()
+        {
+            return "this will be a list of products";
+        }
+
+        [HttpGet("{id}")]
+        public string GetProduct(int id)
+        {
+            return "this will be a product";
+        }
+
+
+    }
+}
