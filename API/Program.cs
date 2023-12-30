ca0b377b0176d341792e316012c08ea9377d8f4f
Ali Akbar Ebrahimi <a.ebrahimi1367@gmail.com>
2023-12-31 00:48:06 +0330

    03_Add new entities and seed data_Configure Auto migration

--- API/Program.cs
+++ API/Program.cs
@@ -1,13 +1,16 @@
+using Core.Interfaces;
+using Infrastructure;
 using Infrastructure.Data;
 using Microsoft.EntityFrameworkCore;
 
 var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
+builder.Services.AddScoped<IProductRepository, ProductRepository>();
 
+// Add services to the container.
 builder.Services.AddControllers();
 
-builder.Services.AddDbContext<StoreContext>(x=>
+builder.Services.AddDbContext<StoreContext>(x =>
     x.UseSqlite(builder.Configuration.GetConnectionString("DefaultConnection")));
 
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
@@ -32,5 +35,22 @@ app.UseHttpsRedirection();
 app.UseAuthorization();
 
 app.MapControllers();
+
+using (var scope = app.Services.CreateScope())
+{
+    var services = scope.ServiceProvider;
+    var loggerFactory = services.GetRequiredService<ILoggerFactory>();
+    try
+    {
+        var context = services.GetRequiredService<StoreContext>();
+        await context.Database.MigrateAsync();
+        await StoreContextSeed.SeedAsync(context,loggerFactory);
+    }
+    catch (Exception ex)
+    {
+        var logger=loggerFactory.CreateLogger<Program>();
+        logger.LogError(ex,"An error occured during migration...");
+    }
+}
 
 app.Run();
