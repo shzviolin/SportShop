302b62113cee3db0a925b62e0b90a70662ce9f0b
Ali Akbar Ebrahimi <a.ebrahimi1367@gmail.com>
2024-01-02 00:39:05 +0330

    06_API Paging Filtering Sorting Searching

--- API/Controllers/ProductController.cs
+++ API/Controllers/ProductController.cs
@@ -27,9 +27,9 @@ namespace API.Controllers
         }
 
         [HttpGet]
-        public async Task<ActionResult<IReadOnlyList<ProductToReturnDto>>> GetProducts()
+        public async Task<ActionResult<IReadOnlyList<ProductToReturnDto>>> GetProducts(string sort, int? brandId, int? typeId)
         {
-            var spec = new ProductsWithTypesAndBrandsSpecification();
+            var spec = new ProductsWithTypesAndBrandsSpecification(sort,brandId,typeId);
             var products = await _productRepo.ListAsync(spec);
             return Ok(_mapper.Map<IReadOnlyList<Product>,IReadOnlyList<ProductToReturnDto>>(products));
         }
--- Infrastructure/Data/SpecificationEvaluator.cs
+++ Infrastructure/Data/SpecificationEvaluator.cs
@@ -8,12 +8,28 @@ namespace Infrastructure.Data
     {
         public static IQueryable<TEntity> GetQuery(
             IQueryable<TEntity> inputQuery,
-            ISpecification<TEntity> spec)
+            ISpecification<TEntity> spec
+            )
         {
             var query = inputQuery;
             if (spec.Criteria is not null)
             {
                 query = query.Where(spec.Criteria);
+            }
+
+            if (spec.OrderBy is not null)
+            {
+                query = query.OrderBy(spec.OrderBy);
+            }
+
+            if (spec.OrderByDescending is not null)
+            {
+                query = query.OrderByDescending(spec.OrderByDescending);
+            }
+
+            if (spec.IsPagingEnabled)
+            {
+                query = query.Skip(spec.Skip).Take(spec.Take);
             }
 
             query = spec.Includes.Aggregate(query, (current, include) => current.Include(include));
